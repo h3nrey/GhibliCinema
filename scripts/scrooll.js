4a8460e2bcfcdca2833b2140aded4c69b10edb4e
henrey22 <pedro.novaes@nave.org.br>
2021-06-23 22:36:21 -0300

    movielist scrool working bleh, header fixed and cards hover ok

--- scripts/scrooll.js
+++ scripts/scrooll.js
@@ -1,5 +1,24 @@
-const cardsList = document.querySelectorAll("#VerticalList");
-cardsList.addEventListener
-function setWidth(){
-    cardsList.width = 2333;
+const movieList = document.querySelector("div.VerticallList");
+
+function mudarLeft() {
+    /* let valorx = movieList.style.marginLeft - Math.round(window.innerWidth / 2);
+    if (movie.style.marginLeft > 0) {      
+        movieList.style.marginLeft = `${valorx}px`;
+    } */
+    movieList.style.marginLeft = '0px';
+}
+
+function mudar() {
+    let valorx = movieList.style.marginRight - Math.round(window.innerWidth / 3);
+    movieList.style.marginLeft = `${valorx}px`;
+}
+
+const arrowLeft = document.querySelectorAll("div.arrow-left");
+for (const iterator of arrowLeft) {
+    iterator.addEventListener('click', mudarLeft);
+}
+
+const arrowRight = document.querySelectorAll("div.arrow-right");
+for (const iterator of arrowRight) {
+    iterator.addEventListener('click', mudar);
 }
